9b9f7ae7fd59b5df10848aaf5fbf37d54b79e0ae
nayeon Kim <kimnayeon574@gmail.com>
2019-05-28 14:45:36 +0900

    #2 ListBox에 항목 추가 , 선택 구현

--- ListBox/Form1.Designer.cs
+++ ListBox/Form1.Designer.cs
@@ -42,6 +42,7 @@
             this.lbView.Name = "lbView";
             this.lbView.Size = new System.Drawing.Size(278, 160);
             this.lbView.TabIndex = 0;
+            this.lbView.SelectedIndexChanged += new System.EventHandler(this.lbView_SelectedIndexChanged);
             // 
             // lblResult
             // 
@@ -58,6 +59,7 @@
             this.txtList.Name = "txtList";
             this.txtList.Size = new System.Drawing.Size(278, 21);
             this.txtList.TabIndex = 2;
+            this.txtList.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtList_KeyPress);
             // 
             // btnAdd
             // 
@@ -68,6 +70,7 @@
             this.btnAdd.TabIndex = 3;
             this.btnAdd.Text = "추가";
             this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
             // 
             // Form1
             // 
@@ -82,6 +85,7 @@
             this.MaximizeBox = false;
             this.Name = "Form1";
             this.Text = "리스트추가";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
--- ListBox/Form1.cs
+++ ListBox/Form1.cs
@@ -11,9 +11,49 @@ namespace ListBox
 {
     public partial class Form1 : Form
     {
+        string OrgStr = "";
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.OrgStr = this.lblResult.Text;
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            TextCheck();
+        }
+
+        private void TextCheck()
+        {
+            if (this.txtList.Text == "")
+            {
+                MessageBox.Show("항목을 입력해주세요.", "알림", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                this.txtList.Focus();
+            }
+            else
+            {
+                this.lbView.Items.Add(this.txtList.Text);
+                this.txtList.Text = "";
+                this.txtList.Focus();
+            }
+        }
+
+        private void txtList_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if(e.KeyChar == 13)
+            {
+                TextCheck();
+                e.Handled = true; // 소리를 안나게 하는 코드
+            }
+        }
+
+        private void lbView_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            this.lblResult.Text = this.OrgStr + this.lbView.SelectedItem;
         }
     }
 }
